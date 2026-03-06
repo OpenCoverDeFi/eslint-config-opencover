0ea5e0ca7a272906fb122d675e40acf5157e37f1
Yury Oparin <yury.oparin@gmail.com>
2026-03-06 21:05:29 +0000

    Move linter and config outside lint and lintAndFix

--- tests/lint.ts
+++ tests/lint.ts
@@ -1,44 +1,27 @@
 import { Linter } from 'eslint';
 import opencover from '@/index.js';
 
-export function lint(code: string, filename: string): Linter.LintMessage[] {
-    const linter = new Linter({ configType: 'flat' });
+const linter = new Linter({ configType: 'flat' });
 
-    const config: Linter.Config[] = [
-        ...opencover,
-        {
-            languageOptions: {
-                parserOptions: {
-                    projectService: {
-                        allowDefaultProject: ['*.ts', '*.tsx', '*.js', '*.jsx', '*.test.ts', '*.test.tsx'],
-                        defaultProject: 'tsconfig.json',
-                    },
-                    tsconfigRootDir: process.cwd(),
+const config: Linter.Config[] = [
+    ...opencover,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: {
+                    allowDefaultProject: ['*.ts', '*.tsx', '*.js', '*.jsx', '*.test.ts', '*.test.tsx'],
+                    defaultProject: 'tsconfig.json',
                 },
+                tsconfigRootDir: process.cwd(),
             },
         },
-    ] as Linter.Config[];
+    },
+] as Linter.Config[];
 
+export function lint(code: string, filename: string): Linter.LintMessage[] {
     return linter.verify(code, config, { filename });
 }
 
 export function lintAndFix(code: string, filename: string): Linter.FixReport {
-    const linter = new Linter({ configType: 'flat' });
-
-    const config: Linter.Config[] = [
-        ...opencover,
-        {
-            languageOptions: {
-                parserOptions: {
-                    projectService: {
-                        allowDefaultProject: ['*.ts', '*.tsx', '*.js', '*.jsx', '*.test.ts', '*.test.tsx'],
-                        defaultProject: 'tsconfig.json',
-                    },
-                    tsconfigRootDir: process.cwd(),
-                },
-            },
-        },
-    ] as Linter.Config[];
-
     return linter.verifyAndFix(code, config, { filename });
 }
