07c534888d2dede5d7f55f61ebdc2544d67166b3
Yury Oparin <yury.oparin@gmail.com>
2026-03-06 20:37:22 +0000

    Move config inside lint and lintAndFix functions in tests

--- tests/lint.ts
+++ tests/lint.ts
@@ -3,25 +3,40 @@ import opencover from '@/index.js';
 
 const linter = new Linter({ configType: 'flat' });
 
-const config: Linter.Config[] = [
-    ...opencover,
-    {
-        languageOptions: {
-            parserOptions: {
-                projectService: {
-                    allowDefaultProject: ['*.ts', '*.tsx', '*.js', '*.jsx', '*.test.ts', '*.test.tsx'],
-                    defaultProject: 'tsconfig.json',
+export function lint(code: string, filename: string): Linter.LintMessage[] {
+    const config: Linter.Config[] = [
+        ...opencover,
+        {
+            languageOptions: {
+                parserOptions: {
+                    projectService: {
+                        allowDefaultProject: ['*.ts', '*.tsx', '*.js', '*.jsx', '*.test.ts', '*.test.tsx'],
+                        defaultProject: 'tsconfig.json',
+                    },
+                    tsconfigRootDir: process.cwd(),
                 },
-                tsconfigRootDir: process.cwd(),
             },
         },
-    },
-] as Linter.Config[];
+    ] as Linter.Config[];
 
-export function lint(code: string, filename: string): Linter.LintMessage[] {
     return linter.verify(code, config, { filename });
 }
 
 export function lintAndFix(code: string, filename: string): Linter.FixReport {
+    const config: Linter.Config[] = [
+        ...opencover,
+        {
+            languageOptions: {
+                parserOptions: {
+                    projectService: {
+                        allowDefaultProject: ['*.ts', '*.tsx', '*.js', '*.jsx', '*.test.ts', '*.test.tsx'],
+                        defaultProject: 'tsconfig.json',
+                    },
+                    tsconfigRootDir: process.cwd(),
+                },
+            },
+        },
+    ] as Linter.Config[];
+
     return linter.verifyAndFix(code, config, { filename });
 }
